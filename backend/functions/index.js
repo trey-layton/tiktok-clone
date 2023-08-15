610bcee8aa875d3a45c9766f93cd4880fb1a41e0
Trey Layton <treylayton@Treys-Air.lan>
2023-08-15 14:10:57 -0500

    Set up authentication services

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,19 +1,13 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require('firebase-functions')
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const admin = require('firebase-admin')
+admin.initializeApp()
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const db = admin.firestore()
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.newUser = functions.auth.user().onCreate((user)=>{
+    return db
+    .collection('user')
+    .doc(user.uid)
+        .create(JSON.parse(JSON.stringify(user)))
+})
